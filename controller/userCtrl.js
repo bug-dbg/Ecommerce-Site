509bd3098d2a29c384b7066ed0b3c7b9a2a40021
Ren <renactub@gmail.com>
2021-11-02 13:23:33 +0800

    added register, login, logout and jwt

--- controller/userCtrl.js
+++ controller/userCtrl.js
@@ -30,25 +30,55 @@ const userCtrl = {
             const accessToken = createAccessToken({id: newUser._id})
             const refreshToken = createRefreshToken({id: newUser._id})
 
-            // res.setHeader('Set-Cookie', cookie.serialize('refreshtoken', String(refreshToken), {
-            //     httpOnly: true,
-            //     path: '/user/refresh_token',
-            // }))
-            res.cookie('refreshtoken', refreshToken, {
+            res.cookie('refreshToken', refreshToken, {
                 httpOnly: true,
                 path: '/user/refresh_token',
                 maxAge: 7*24*60*60*1000 // 7 days
             })
             res.json({accessToken})
-            //res.json({msg: "Register Sucess!"})
 
         } catch (err) {
             return res.status(500).json({msg: err.message})
       }
     }, 
+    login: async (req, res) => {
+        try{
+            const {email, password} = req.body
+
+            const user = await Users.findOne({email})
+            if(!user) return res.status(400).json({msg: "User does not exist."})
+
+            const isMatch = await bcrypt.compare(password, user.password)
+            if(!isMatch) return res.status(400).json({msg: "Incorrect password."})
+
+            // if login is successful, create an accesstoken and refreshtoken
+
+            const accessToken = createAccessToken({id: user._id})
+            const refreshToken = createRefreshToken({id: user._id})
+
+            res.cookie('refreshToken', refreshToken, {
+                httpOnly: true,
+                path: '/user/refresh_token',
+                maxAge: 7*24*60*60*1000 // 7 days
+            })
+            res.json({accessToken})
+        
+        } catch (err) {
+            return res.status(500).json({msg: err.message})
+        }
+    },
+    logout: async (req, res) => {
+        try {
+            res.clearCookie('refreshToken', {path: '/user/refresh_token'})
+            return res.json({msg: "Logged out"})
+        } catch (err) {
+            return res.status(500).json({msg: err.message})
+        }
+    },
     refreshToken: (req, res) => {
         try {
             const rf_token = req.cookies.refreshToken;
+            
             if(!rf_token) return res.status(400).json({msg: "Please Login or Register"})
 
             jwt.verify(rf_token, process.env.REFRESH_TOKEN_SECRET, (err, user) => {
@@ -67,7 +97,7 @@ const userCtrl = {
 }
 
 const createAccessToken = (user) =>{
-    return jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, {expiresIn: '11m'})
+    return jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, {expiresIn: '1d'})
 }
 const createRefreshToken = (user) =>{
     return jwt.sign(user, process.env.REFRESH_TOKEN_SECRET, {expiresIn: '7d'})
